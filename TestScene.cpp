0325340a6c9bb82d7e7cfcd98e6aa1f47c440edd
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-02-22 10:30:02 +0100

    LAB01 Handson part 1
    Added gameobjects to the testscene
    Removed the log.txt file
    Added log.txt to the .gitignore

--- TestScene.cpp
+++ TestScene.cpp
@@ -1,16 +1,39 @@
 #include "stdafx.h"
 #include "TestScene.h"
 
+#include "CubePosColorNorm.h"
+#include "TorusPosColorNorm.h"
+#include "CylinderPosColorNorm.h"
 #include "Logger.h"
 
 void TestScene::Initialize()
 {
+	// Create a cube object
+	m_pBox = new CubePosColorNorm{ 5.0f, 2.0f, 1.0f };
+	AddGameObject(m_pBox);
 
+	// Translate the cube
+	m_pBox->Translate(0.0f, 1.0f, 0.0f);
+
+	// Camera setup
+	m_SceneContext.GetCamera()->SetPosition(XMFLOAT3{ 0.0f, 10.0f, -25.0f });
+	m_SceneContext.GetCamera()->SetForward(XMFLOAT3{ 0.0f, -0.3f, 1.0f });
+
+	// Create a torus object
+	const auto pTorus{ new TorusPosColorNorm{ 8.0f, 50, 1.0f, 10, XMFLOAT4{ Colors::Crimson } } };
+	AddGameObject(pTorus);
+
+	// Create a cylinder object
+	const auto pCylinder{ new CylinderPosColorNorm{ 1.5f, 10, 4.0f } };
+	pTorus->AddChild(pCylinder);
+	pCylinder->Translate(0.0f, 8.0f, 0.0f);
 }
 
 void TestScene::Update()
 {
-
+	// Rotate the cube over time
+	const float totalTime = m_SceneContext.GetGameTime()->GetTotal();
+	m_pBox->RotateDegrees(0.0f, 90.0f * totalTime, 0.0);
 }
 
 void TestScene::Draw() const
